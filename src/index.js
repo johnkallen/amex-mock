6803c9a78e49a9937b8f34cb7a634802089af200
JCBenR <jsieb81@gmail.com>
2022-05-11 13:23:44 -0600

    added header

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import MiscBox from './MiscBox';
 import Footer from './Footer';
 import MiscBoxContainer from './MiscBoxContainer';
+import Header from './Header';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
     <App />
     <MiscBoxContainer />
     <Footer/>
