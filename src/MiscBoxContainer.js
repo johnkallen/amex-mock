91150a74408b423765371df19c268422739b8bdf
Omar Ali <omar.k.ali217@gmail.com>
2022-05-11 15:17:10 -0400

    misc box row styling

--- src/MiscBoxContainer.js
+++ src/MiscBoxContainer.js
@@ -6,30 +6,32 @@ import MiscBox from './MiscBox';
 
 function MiscBoxContainer(props) {
     return (
-         <div className='grid aem-GridColumn aem-GridColumn--default--12'>
-            <div className='servicing-title-grid'>   
-                <div className="card-img-bg" />
-                <div className='container'>
-                    <div className='row pad-3-t pad-3-b'>
-                        <MiscBox 
-                            image="./pointer.png"
-                            title="Places to Use Your Card"
-                            description="Check out businesses that accept Amex"
-                            buttonText="Explore Now"/>
-                        <MiscBox 
-                            image="./creditScore.png"
-                            title="Free Credit Score"
-                            description="Get a detailed TransUnion credit report that helps you stay informed"
-                            buttonText="View Now"/>
-                        <MiscBox 
-                            image="./creditHand.png"
-                            title="Start Accepting Amex"
-                            description="Get access to 112MM+ Cards WorldWide and competitive pricing opportunities"
-                            buttonText="Learn More"/>
-                    </div>
-                </div>
-            </div>
+        //  <div className='grid aem-GridColumn aem-GridColumn--default--12'>
+        //     {/* <div className='servicing-title-grid'>    */}
+        //         {/* <div className="card-img-bg" /> */}
+        //         <div className='container'>
+                
+        //         {/* </div> */}
+        //     </div>
 
+        // </div>
+
+        <div className='misc-flex'>
+            <MiscBox
+                image="./pointer.png"
+                title="Places to Use Your Card"
+                description="Check out businesses that accept Amex"
+                buttonText="Explore Now" />
+            <MiscBox
+                image="./creditScore.png"
+                title="Free Credit Score"
+                description="Get a detailed TransUnion credit report that helps you stay informed"
+                buttonText="View Now" />
+            <MiscBox
+                image="./creditHand.png"
+                title="Start Accepting Amex"
+                description="Get access to 112MM+ Cards WorldWide and competitive pricing opportunities"
+                buttonText="Learn More" />
         </div>
     );
 }
