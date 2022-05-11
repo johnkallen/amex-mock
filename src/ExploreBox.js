80108dc489e7fdff3ae4361796fcbe7b85dc543f
JCBenR <jsieb81@gmail.com>
2022-05-11 12:53:38 -0600

    main section changes and app icons

--- src/ExploreBox.js
+++ src/ExploreBox.js
@@ -4,8 +4,8 @@ import './amex.css'
 
 export default function ExploreBox(props) {
   return (
-    <div>
-        <img src={props.image} alt="" />
+    <div className='ExploreBox'>
+        <img className='exploreImage' src={props.image} alt="" />
         <h3>Explore Lending Options</h3>
         <button className='btn'>Visit Now</button>
     </div>
--- src/MainSection.js
+++ src/MainSection.js
@@ -3,13 +3,17 @@ import ImageSectionLarge from './ImageSectionLarge'
 import ImageSectionSmall from './ImageSectionSmall'
 import './MainSection.css'
 
-export default function  
-MainSection() {
+export default function
+  MainSection() {
   return (
     <div className='main'>
-        <div className="leftHolder">Selector Holder</div>
-        <ImageSectionLarge/>
-        <ImageSectionSmall/>
+      <div className="leftHolder">Selector Holder</div>
+      <div className="centerHolder">
+        <ImageSectionLarge />
+      </div>
+      <div className="rightHolder">
+        <ImageSectionSmall />
+      </div>
     </div>
   )
 }
