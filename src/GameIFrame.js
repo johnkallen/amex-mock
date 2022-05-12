cf8f48b20977dccb9206f3fbc6e7bff1bc841459
John Kallen <johnckallen@gmail.com>
2022-05-12 08:16:48 -0600

    added Images

--- src/GameIFrame.js
+++ src/GameIFrame.js
@@ -3,59 +3,64 @@ import './amex.css';
 import './GameIFrame.css';
 import $ from 'jquery'
 import React from 'react';
+import invaders from './Images/play_pay_invaders.png';
 
 class GameIFrame extends React.Component {
 
     componentDidMount() {
 
-        $(function () {
-            $("#frameButton").on("click", fullscreenClick)
+        // $(function () {
+        //     $("#frameButton").on("click", fullscreenClick)
 
-            function fullscreenClick() {
-                //The button is this
-                //We want to clone the parent
-                var box = $("#myframe")
-                //create a holder box so the layout stays the same
-                var holder = $(box).clone(false, true);
-                //and make it not visible
-                $(holder).css({
-                    "visibility": "hidden"
-                });
+        //     function fullscreenClick() {
+        //         //The button is this
+        //         //We want to clone the parent
+        //         var box = $("#myframe")
+        //         //create a holder box so the layout stays the same
+        //         var holder = $(box).clone(false, true);
+        //         //and make it not visible
+        //         $(holder).css({
+        //             "visibility": "hidden"
+        //         });
 
-                //Get its position
-                var pos = $(box).position();
+        //         //Get its position
+        //         var pos = $(box).position();
 
-                //Substitute our box with our holder
-                $(box).before($(holder));
+        //         //Substitute our box with our holder
+        //         $(box).before($(holder));
 
-                //Set the position of our box (not holder)
-                //Give it absolute position (eg. outside our set structure)
-                $(box).css({
-                    "position": "absolute",
-                    "left": pos.left + "px",
-                    "top": pos.top + "px",
-                    "z-index": 1000,
-                    "overflow": "hidden",
-                    "height": "100vh"
-                });
+        //         //Set the position of our box (not holder)
+        //         //Give it absolute position (eg. outside our set structure)
+        //         $(box).css({
+        //             "position": "absolute",
+        //             "left": pos.left + "px",
+        //             "top": pos.top + "px",
+        //             "z-index": 1000,
+        //             "overflow": "hidden",
+        //             "height": "100vh"
+        //         });
 
-                //Set class so it can be animated
-                $(box).addClass("fullscreen");
+        //         //Set class so it can be animated
+        //         $(box).addClass("fullscreen");
 
-                //Animate the position
-                $(box).animate({
-                    "top": 0,
-                    "left": 0,
-                }, 5000);
-            }
-        });
+        //         //Animate the position
+        //         $(box).animate({
+        //             "top": 0,
+        //             "left": 0,
+        //         }, 5000);
+        //     }
+        // });
     }
 
     render() {
 
         return (
             <div className='gameFrame'>
-                <iframe id="myframe" src='https://www.sandy.utah.gov'></iframe>
+                {/* <iframe id="myframe" src='http://localhost:3000/'></iframe> */}
+                <div>
+                    <img src="./Images/pay_invaders.png" height="200" width="300" href="http://localhost:3000/" />
+                    bogus
+                </div>
 
                 <div id='frameButton'>
                     <div className="picText">
